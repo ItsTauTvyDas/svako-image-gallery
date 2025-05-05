5bb768e0cdc35a96f531f51ffc3decea57bf2fa2
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-06 01:02:57 +0300

    Add account data report route and rename some other routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Livewire\Dashboard\AccountDataReport;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.main');
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
@@ -14,11 +15,13 @@ Route::view('dashboard', 'dashboard')
     ->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+    Route::redirect('settings', 'dashboard/settings/profile');
 
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    Route::get('dashboard/data-report', AccountDataReport::class)->name('dashboard.data-report');
+
+    Route::get('dashboard/settings/profile', Profile::class)->name('settings.profile');
+    Route::get('dashboard/settings/password', Password::class)->name('settings.password');
+    Route::get('dashboard/settings/appearance', Appearance::class)->name('settings.appearance');
 });
 
 require __DIR__.'/auth.php';
