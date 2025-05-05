ce12715c3df82bf883ffde21f35b38a964ae697b
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-06 01:03:16 +0300

    Define resources in vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,14 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/bootstrap.min.css',
+                'resources/css/main-style.css',
+                'resources/js/bootstrap.bundle.min.js',
+                'resources/js/app.js',
+                'resources/js/color-modes.js',
+            ],
             refresh: [`resources/views/**/*`],
         }),
         tailwindcss(),
