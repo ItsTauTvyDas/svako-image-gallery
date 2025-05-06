8a9babc40078146bc11b8bc4b9c3a5eee48b1ff2
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-06 04:03:55 +0300

    Add prefix to route

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,20 @@ Route::get('/', function () {
     return view('pages.main');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::prefix('dashboard')->group(function () {
+    Route::view('/', 'dashboard')
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'dashboard/settings/profile');
+    Route::middleware(['auth'])->group(function () {
+        Route::redirect('settings', 'dashboard/settings/profile');
 
-    Route::get('dashboard/data-report', AccountDataReport::class)->name('dashboard.data-report');
+        Route::get('data-report', AccountDataReport::class)->name('dashboard.data-report');
 
-    Route::get('dashboard/settings/profile', Profile::class)->name('settings.profile');
-    Route::get('dashboard/settings/password', Password::class)->name('settings.password');
-    Route::get('dashboard/settings/appearance', Appearance::class)->name('settings.appearance');
+        Route::get('settings/profile', Profile::class)->name('settings.profile');
+        Route::get('settings/password', Password::class)->name('settings.password');
+        Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    });
 });
 
 require __DIR__.'/auth.php';
