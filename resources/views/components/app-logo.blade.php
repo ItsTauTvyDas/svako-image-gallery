1bcb8bc3db1ff6a4f7aa591f948a0884a2fb55e0
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-06 04:03:02 +0300

    Modify app logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,8 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
-</div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
-</div>
+<a href="{{ route('home') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse link-body-emphasis text-decoration-none" wire:navigate>
+    <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+        <flux:icon.camera variant="solid"/>
+    </div>
+    <div class="ms-1 grid flex-1 text-start text-lg">
+        <span class="mb-0.5 truncate leading-none font-semibold">{{ __('Nuotraukų Galerija') }}</span>
+    </div>
+</a>
