87874b487fc8d6e99d87655cdc0f593d773b431a
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-06 04:05:09 +0300

    Add if check

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,10 +1,23 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? 'Paveikslų galerija' }}</title>
+<title>{{ ($title ? "$title - " : '') . __('Paveikslų Galerija') }}</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite([
+    'resources/css/app.css',
+    'resources/js/app.js',
+])
+
+@if (!request()->is('dashboard*') && !request()->is('system/*'))
+    @vite([
+        'resources/css/bootstrap.min.css',
+        'resources/js/color-modes.js',
+        'resources/css/main-style.css',
+        'resources/js/bootstrap.bundle.min.js'
+    ])
+@endif
+
 @fluxAppearance
