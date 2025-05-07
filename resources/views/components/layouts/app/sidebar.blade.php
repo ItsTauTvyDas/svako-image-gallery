fcdaf62a123c663370f891b676989d48231c8848
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-07 03:25:09 +0300

    Update logo display

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -6,11 +6,7 @@
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky stashable class="border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
-
-            <a href="{{ route('dashboard') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse" wire:navigate>
-                <x-app-logo />
-            </a>
-
+            <x-app-logo />
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Pagrindinis')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Valdymo skydas') }}</flux:navlist.item>
