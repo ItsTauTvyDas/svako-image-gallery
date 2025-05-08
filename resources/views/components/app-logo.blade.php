b3fb854a84dc6bd6b5b288bfe9eea8945abca87b
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-08 08:43:31 +0300

    Remove wire:navigate

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('home') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse link-body-emphasis text-decoration-none" wire:navigate>
+<a href="{{ route('home') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse link-body-emphasis text-decoration-none">
     <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
         <flux:icon.camera variant="solid"/>
     </div>
