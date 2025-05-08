6d31f81f7f994a61f67b5ce1af791182338c1a0e
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-08 08:40:36 +0300

    Add upload rules page layout

---
+++ resources/views/livewire/pages/upload-rules.blade.php
@@ -0,0 +1,46 @@
+<main>
+    <div class="py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="card shadow-sm">
+                        <div class="card-header">
+                            <h3 class="mb-0">Nuotraukų įkėlimo taisyklės</h3>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                Mūsų puslapyje leidžiama įkelti tik saugų turinį. Prieš įkeldami
+                                nuotraukas, susipažinkite su šiomis taisyklėmis:
+                            </p>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <strong>Leidžiami formatai:</strong> <span class="badge bg-success">JPEG</span>,
+                                    <span class="badge bg-success">PNG</span>, <span class="badge bg-success">GIF</span>
+                                </li>
+                                <li class="list-group-item">
+                                    <strong>Maksimalus dydis:</strong> 25 MB vienai nuotraukai
+                                </li>
+                                <li class="list-group-item">
+                                    <strong>NSFW draudžiama:</strong> jokio jaunimui nesaugaus ar kitokio
+                                    neleistino turinio
+                                </li>
+                                <li class="list-group-item">
+                                    <strong>Autorių teisės:</strong> tik savo arba laisvai platinamas
+                                    (angl. <em>public domain</em> / <em>CC0</em>) nuotraukas
+                                </li>
+                                <li class="list-group-item">
+                                    <strong>Sauga:</strong> visos įkeltos nuotraukos automatiškai
+                                    skenuojamos antivirusine programine įranga
+                                </li>
+                                <li class="list-group-item">
+                                    <strong>Privatumas:</strong> neišlaikome jokios asmeninės informacijos
+                                    iš jūsų nuotraukų EXIF metažymose
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
