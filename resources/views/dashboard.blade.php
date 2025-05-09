16fe644fc791fd958318094baaf901bd4a82e64d
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-09 08:01:18 +0300

    Add basic dashboard welcome message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,9 @@
 <x-layouts.app :title="__('Valdymo skydas')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
+    <section class="w-full">
+        <div class="relative w-full">
+            <flux:heading size="xl" level="1">{{ __('Valdymo skydas') }}</flux:heading>
+            <flux:subheading size="lg" class="mb-6">{{ __('Čia rasite savo paskyros nustatymus!') }}</flux:subheading>
+            <flux:separator variant="subtle" />
         </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-        </div>
-    </div>
+    </section>
 </x-layouts.app>
