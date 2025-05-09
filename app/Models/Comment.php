30b0eb471c11e8766a7f4e82dbac1d6b69960ede
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-09 04:50:02 +0300

    Use BelongsTo instead of the model class

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @method static find(int $markForDeletionId)
+ */
 class Comment extends Model
 {
     protected $table = 'post_comments';
     protected $fillable = ['post_id', 'user_id', 'content'];
 
-    public function author(): null|User
+    public function author(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_id')->first();
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
