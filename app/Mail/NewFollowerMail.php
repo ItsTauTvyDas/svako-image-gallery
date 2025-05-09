993866a77032e23366b0aec9da668b36edef20bb
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-09 06:24:30 +0300

    Add new follower mail

---
+++ app/Mail/NewFollowerMail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewFollowerMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public User $user, $follower;
+
+    public function __construct(User $user, User $follower)
+    {
+        $this->user = $user;
+        $this->follower = $follower;
+    }
+
+    public function build(): NewFollowerMail
+    {
+        return $this->from(config('mail.from.address'))
+            ->subject(__('Naujas sekėjas!'))
+            ->text('emails.new-follower');
+    }
+
+}
