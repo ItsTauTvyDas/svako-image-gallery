b7e69605edf4112187e5f2cb8bda79a4c96c9c99
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-09 04:53:25 +0300

    Add likes() and comments()

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Storage;
 
 /**
  * @method static where(string $string, string $string1, string $string2)
+ * @method static whereRaw(string $string, string[] $array)
  */
 class Post extends Model
 {
@@ -19,9 +20,19 @@ class Post extends Model
         return $this->belongsTo(User::class, 'user_id')->first();
     }
 
-    public function getLikeCount(): int
+    public function likeCount(): int
     {
-        return $this->hasMany(PostLike::class)->count();
+        return $this->likes()->count();
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(PostLike::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 
     protected static function booted(): void
