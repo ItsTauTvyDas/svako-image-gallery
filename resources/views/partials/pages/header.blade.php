1c257d4c3e0b25876e6d37f05a1de86dcfaece0a
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-09 04:52:32 +0300

    Finish header links

--- resources/views/partials/pages/header.blade.php
+++ resources/views/partials/pages/header.blade.php
@@ -1,17 +1,17 @@
 <header class="bg-body-tertiary py-3 mb-4 border-bottom">
     <div class="container d-flex flex-wrap align-items-center justify-content-center justify-content-md-between">
         <div class="col-md-3 mb-md-0">
-            <x-app-logo />
+            <x-app-logo/>
         </div>
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
             <li>
-                <a href="#" class="nav-link px-2 link-secondary">{{ __('Pagrindinis') }}</a>
+                <a href="{{ route('home') }}" @class(['nav-link px-2', 'link-secondary' => request()->routeIs('home')])>{{ __('Pagrindinis') }}</a>
             </li>
             <li>
-                <a href="#" class="nav-link px-2">{{ __('Vartotojai') }}</a>
+                <a href="{{ route('users-list') }}" @class(['nav-link px-2', 'link-secondary' => request()->routeIs('users-list')])>{{ __('Vartotojai') }}</a>
             </li>
             <li>
-                <a href="#" class="nav-link px-2">{{ __('Populiarūs Vaizdai') }}</a>
+                <a href="{{ route('upload-rules') }}" @class(['nav-link px-2', 'link-secondary' => request()->routeIs('upload-rules')])>{{ __('Taisyklės') }}</a>
             </li>
             <li>
                 <a href="#" class="nav-link px-2">{{ __('Apie') }}</a>
@@ -20,10 +20,13 @@
         <div class="col-md-3 text-end">
             @auth
                 <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        {{ auth()->user()->name }}
+                    <button class="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <flux:icon.user variant="mini" class="d-inline"/> {{ auth()->user()->name }}
                     </button>
                     <ul class="dropdown-menu">
+                        <li>
+                            <a class="dropdown-item" href="{{ route('profile', ['id' => auth()->id()]) }}">{{ __('Viešas profilis') }}</a>
+                        </li>
                         <li>
                             <a class="dropdown-item" href="{{ route('dashboard') }}">{{ __('Valdymo skydas') }}</a>
                         </li>
